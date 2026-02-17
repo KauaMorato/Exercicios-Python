d485e4420e2a48347c51d4a05fa70afb5775fb02
KauaMorato <dxrkitslove4@gmail.com>
2026-02-17 15:34:02 -0300

    Update and rename Exercício6.py to Exercício06.py

---
+++ Exercício06.py
@@ -0,0 +1,5 @@
+# Dobro, Triplo, Raiz Quadrada
+
+n = int(input('Um número: '))
+
+print(f'\n O dobro de {n} vale {n * 2} \n O triplo de {n} vale {n * 3} \n A raiz quadrada de {n} vale {n ** (1/2):.2f}')
